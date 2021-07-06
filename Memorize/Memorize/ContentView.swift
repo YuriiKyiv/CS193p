a712f17256a124f869c6354e93a6bed3e28c3ae2
Yurii Trokhymchuk <yurii.trokhymchuk@skylum.com>
2021-07-06 15:25:37 +0300

    complete lecture 1

---
+++ Memorize/Memorize/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Yurii Trokhymchuk on 11.06.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(lineWidth: 3)
+            Text("Hello, world!")
+        }
+        .padding(.horizontal)
+        .foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
+    }
+}
+
+
+
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
